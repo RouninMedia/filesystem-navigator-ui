4d20f6ca0e50795b48dcbf13d66b21fbdf339119
Alan Lansdowne <alan_lansdowne@yahoo.com>
2024-03-14 19:21:29 +0000

    Update filesystem-navigator-ui.js

--- filesystem-navigator-ui.js
+++ filesystem-navigator-ui.js
@@ -1,14 +1,15 @@
 const webDirectoryFolderNames = document.querySelectorAll('.webDirectoryFolderName');
- 
+
 const toggleFolder = (e) =>  {
   const webDirectory = e.currentTarget.nextElementSibling;
   webDirectory.style.setProperty('transition', `height ${(webDirectory.scrollHeight / 360).toFixed(2)}s linear`);
   webDirectory.style.setProperty('height', webDirectory.scrollHeight + 'px');
-  webDirectory.classList.toggle('--open');
   webDirectory.classList.add('--transitioning');
+  webDirectory.classList.toggle('--open');
   const timeout = ((webDirectory.scrollHeight / 360) * 1000);
-  setTimeout(() => webDirectory.classList.remove('--transitioning'), timeout);
   setTimeout(() => webDirectory.style.removeProperty('height'), (webDirectory.classList.contains('--open')) ? timeout : 0);
+  setTimeout(() => webDirectory.classList.remove('--transitioning'), timeout);
+  setTimeout(() => webDirectory.removeAttribute('style'), timeout);
 }
   
 webDirectoryFolderNames.forEach((folderName) => folderName.addEventListener('click', toggleFolder));
